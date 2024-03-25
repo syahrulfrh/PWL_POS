2478a598e4572eb4c7e824c2d8f36e02833890b6
achmad syahrul faroh <syahrulfrh@gmail.com>
2024-03-25 22:15:04 +0700

    JOBSHEET 3 PRAKTIKUM 4

---
+++ resources/views/level.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Data level Pengguna</title>
+</head>
+<body>
+    <h1>Data level pengguna</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>kode level</th>
+            <th>Nama level</th>
+
+        </tr>
+        @foreach ($data as $d)
+            <tr>
+                <td>{{$d ->level_id}}</td>
+                <td>{{$d ->level_kode}}</td>
+                <td>{{$d ->level_nama}}</td>
+
+            </tr>
+        @endforeach
+
+    </table>
+    
+</body>
+</html>

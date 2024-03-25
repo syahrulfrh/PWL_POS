0e100c6e9ca717cd130a2f4d314b6cfbe8369a3f
achmad syahrul faroh <syahrulfrh@gmail.com>
2024-03-26 00:28:22 +0700

    JOBSHEET 4 PRAKTIKUM 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,11 +11,16 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOr(20,['username','nama'],function() {
-            abort(404);
-            });
+        $user = UserModel::where('username','manager9')->firstOrFail();
         return view('user',['data' => $user]);
 
+        //$user = UserModel::findOrFail(1);
+        //return view('user',['data' => $user]);
+
+        //$user = UserModel::findOr(20,['username','nama'],function() {
+        //    abort(404);
+        //    });
+        //return view('user',['data' => $user]);
 
         //$user = UserModel::firstWhere('level_id',1);
         //return view('user',['data' => $user]);

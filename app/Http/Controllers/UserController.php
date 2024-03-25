acdc3a1478473e8b7fcd3d52ff48b8330d00804a
achmad syahrul faroh <syahrulfrh@gmail.com>
2024-03-26 00:11:20 +0700

    JOBSHEET 4 PRAKTIKUM 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,27 +11,43 @@ class UserController extends Controller
 {
     public function index()
     {
+         //$data = [
+         //        'level_id' => 2,
+         //        'username' => 'manager_dua',
+         //        'nama' => 'manager 2' ,
+         //        'password' => Hash::make('12345'),
+                
+         //   ];
+          $data = [
+                'level_id' => 2,
+                'username' => 'manager_tiga',
+                'nama' => 'manager 3' ,
+                'password' => Hash::make('12345'),
+               
+            ];
+             UserModel::create($data);
+
+             $user = UserModel::all();
+             return view('user',['data' => $user]);
+
         //tambah data user dengan Eloquent Model
-        $data =[
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username','customer-1')->update($data);   
+        //$data =[
+        //    'nama' => 'Pelanggan Pertama',
+        //];
+        //UserModel::where('username','customer-1')->update($data);   
         
         //coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user',['data' =>$user]);
+        //$user = UserModel::all(); // ambil semua data dari tabel m_user
+        //return view('user',['data' =>$user]);
 
         //tambah data user dengan eloquent model
-        $data = [
-             'username' => 'customer-1',
-             'nama' => 'pelanggan' ,
-             'password' => Hash::make('12345'),
-             'level_id' => 4
-        ];
-        UserModel::insert($data); //tambahkan data ke tabel m_user
+        //$data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'pelanggan' ,
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        //];
+        //UserModel::insert($data); //tambahkan data ke tabel m_user
 
-        //coba akses model UserModel
-        $user = UserModel::all();
-        return view('user',['data' => $user]);
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,4 +10,5 @@ class UserModel extends Model
     use HasFactory;
     protected $table = 'm_user';  //mendifiniiskan nama tabel yang digunakan oleh model ini
     protected $primaryKey ='user_id'; //mendefinisikan primary key dari nama tabel yang digunakan
-}
+    protected $fillable = ['level_id','username','nama',];
+    }

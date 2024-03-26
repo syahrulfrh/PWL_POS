579580293bd763a35fa4a7ae8fd62a4ff02ab1c0
achmad syahrul faroh <syahrulfrh@gmail.com>
2024-03-26 10:21:13 +0700

    JOBSHEET 4 PRAKTIKUM 2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,11 +11,47 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('level_id',2)->count();
-        dd($user);
+        $user = UserModel::firstOrNew(
+        [
+            'username' => 'manager 33',
+        'nama' => 'Manager Tiga Tiga',
+        'password'=>Hash::make('12345'),
+        'level_id'=>2
+        ],
+    );
+        $user->save();
+        
         return view('user',['data' => $user]);
     }
 }
+        
+        
+        //$user = UserModel::firstOrNew(
+        //    [
+        //        'username' => 'manager',
+        //        'nama' => 'Manager',
+        //    ],
+        //);
+        
+        // $user =UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua ',
+        //         'password'=>Hash::make('12345'),
+        //         'level_id' =>2
+        //     ],
+        // );
+
+        //[
+        //    'username' => 'manager',
+        //    'nama' => 'Manager',
+        //]
+        //);
+        //return view('user',['data' => $user]);
+
+        //$user = UserModel::where('level_id',2)->count();
+        //dd($user);
+        //return view('user',['data' => $user]);
         
         //$user = UserModel::where('username','manager9')->firstOrFail();
         //return view('user',['data' => $user]);

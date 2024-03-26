23b3756764c4ea241157deab3cc35d4764245085
achmad syahrul faroh <syahrulfrh@gmail.com>
2024-03-26 10:37:25 +0700

    JOBSHEET 4 PRAKTIKUM 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,21 +11,65 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-        [
-            'username' => 'manager 33',
-        'nama' => 'Manager Tiga Tiga',
-        'password'=>Hash::make('12345'),
-        'level_id'=>2
-        ],
-    );
-        $user->save();
-        
-        return view('user',['data' => $user]);
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash::make('12345'),
+             'level_id' => 2,
+             ],
+         );
+
+         $user->username = 'manager12';
+
+         $user->save();
+       
+         $user->wasChanged();//true
+         $user->wasChanged('username');//true
+         $user->wasChanged(['username','level_id']);//true
+         $user->wasChanged('nama');//flase
+         dd($user->wasChanged(['nama','username']));//true
+        }
     }
-}
+
+
+
+        //$user = UserModel::create([
+        //    'username' => 'manager55',
+        //    'nama' => 'Manager55',
+        //    'password' => Hash::make('12345'),
+        //    'level_id' => 2,
+        //]);
         
+        //$user->username = 'manager56';
+
+        //$user->isDirty();//true
+        //$user->isDirty('username');//true
+        //$user->isDirty('nama');//false
+        //$user->isDirty(['nama','username']);//true
+
+        //$user->isClean();//flase
+        //$user->isClean('username');//flase
+        //$user->isClean('nama');//true
+        //$user->isClean(['nama','username']);//flase
+
+        //$user->save();
+
+        //$user->isDirty();//false
+        //$user->isClean();//true
+        //dd($user->isDirty());
+
+    //    $user = UserModel::firstOrNew(
+    //    [
+    //       'username' => 'manager 33',
+    //    'nama' => 'Manager Tiga Tiga',
+    //    'password'=>Hash::make('12345'),
+    //    'level_id'=>2
+    //    ],
+    //);
+        //$user->save();
         
+        //return view('user',['data' => $user]);
+
         //$user = UserModel::firstOrNew(
         //    [
         //        'username' => 'manager',
